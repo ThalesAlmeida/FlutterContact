c894e981ca884fd933b3d57be721175adc63bb0f
Thales <thalesalves44@gmail.com>
2018-12-04 23:54:59 -0300

    criando tela de alteração de dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:contatos/ui/contact_page.dart';
 import 'package:flutter/material.dart';
 import 'package:contatos/ui/home_page.dart';
 
 void main(){
   runApp(MaterialApp(
-    home: HomePage(),
+    home: ContactPage(),
+    debugShowCheckedModeBanner: false,
   ));
 }
---
+++ lib/ui/contact_page.dart
@@ -0,0 +1,44 @@
+import 'package:contatos/helpers/contact_helper.dart';
+import 'package:flutter/material.dart';
+
+class ContactPage extends StatefulWidget {
+  final Contact contact;
+
+  ContactPage({this.contact});
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+
+  Contact _editedContact;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    if (widget.contact == null){
+      _editedContact = Contact();
+    }else{
+      _editedContact = Contact.fromMap(widget.contact.toMap());
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text(_editedContact.name ?? "Novo Contato"),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+        child: Icon(Icons.save),
+        backgroundColor: Colors.deepPurple,
+      ),
+    );
+  }
+}
